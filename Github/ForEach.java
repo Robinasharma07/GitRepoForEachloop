27d1b4692eaa58cf87d69e7caa04dec56e718ffa
Robinasharma07 <132258975+Robinasharma07@users.noreply.github.com>
2023-05-01 17:44:34 +0530

    Add files via upload

---
+++ Github/ForEach.java
@@ -0,0 +1,13 @@
+class ForEach{
+	public static void main(String[]args){
+int []a ={10,20,30,40,50};  //it should add[] either  before a(Array) or after
+for (int b : a){
+	System.out.println(b +" ");
+}
+
+}
+	
+	}
+
+
+
